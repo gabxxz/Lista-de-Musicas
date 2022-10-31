b8c2a0ed05d081559ea5e25248ae24623b3e4d2a
gabxxz <gvalente182@gmail.com>
2022-10-31 21:27:01 +0000

    Flask instalado

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Minha primeira aplicação web')
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello,Web!'
+
+def bye():
+
+app.run(debug=True)
