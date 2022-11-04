abefa30a7ac90068df17a9343dbda4842ef2ec74
gabxxz <gvalente182@gmail.com>
2022-11-04 21:03:06 +0000

    home html

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask 
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello,Web!'
-
+    return render_template('home.html')
+    
+@app.route('/bye')
 def bye():
+    return 'Bye'
 
 app.run(debug=True)
